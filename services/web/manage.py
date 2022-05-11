a4c69527e131a5d5153e43871697e4c0c9d79ce3
cameronbosch <72839795+cameronbosch@users.noreply.github.com>
2022-05-11 15:51:28 -0400

    Version 0.8

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,6 +1,5 @@
 from flask.cli import FlaskGroup
-from sqlalchemy import create_engine, Table, Column, String, Integer, Numeric, Text
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from project import app, db
 import psycopg2
@@ -20,74 +19,15 @@ def create_db():
 
 @cli.command("seed_db")
 def seed_db():
-    db.engine.execute(f'DROP TABLE IF EXISTS restaurants;')
+    db.engine.execute('DROP TABLE IF EXISTS restaurants;')
     db.session.commit()
     df = pds.read_csv("restaurants.csv")
     print(df.shape)
     engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
     df.to_sql("restaurants", engine, index=False)
-    
-    
-    class Restaurant(base):
-        __tablename__ = "restaurants"
-
-        id = Column(Integer, primary_key=True)
-        name = Column(String(200), nullable=False)
-        boro = Column(String(60), nullable=False)
-        building = Column(String(30))
-        street = Column(String(150))
-        zip_code = Column(Numeric(5, 0))
-        phone = Column(Numeric(10, 0))
-        cuisine_description = Column(String(60))
-        inspection_date = Column(String)
-        action = Column(Text())
-        violation_code = Column(String(3))
-        violation_description = Column(Text())
-        critical_flag = Column(String(25))
-        score = Column(Numeric(3, 0))
-        grade = Column(String(2))
-        grade_date = Column(String)
-        record_date = Column(String)
-        inspection_type = Column(Text())
-        latitude = Column(Numeric(16, 14))
-        longitude = Column(Numeric(16, 14))
-        community_board = Column(Numeric(3, 0))
-        council_district = Column(Numeric(3, 0))
-        census_tract = Column(Numeric(5, 0))
-    df.to_sql("restaurants", db, schema="restaurant_db", if_exists="append")
-    Session = sessionmaker(db)
-    session = Session()
-    session.add()
-    session.commit()
+    db.session.commit()
 
 
-# if __name__ == "__main__":
-    # cli()
-
-# class Restaurant(db.Model):
-    # __tablename__ = "restaurants"
-
-    # id = db.Column(db.Integer, primary_key=True)
-    # name = db.Column(db.String(200), nullable=False)
-    # boro = db.Column(db.String(60), nullable=False)
-    # building = db.Column(db.String(30))
-    # street = db.Column(db.String(150))
-    # zip_code = db.Column(db.Numeric(5, 0))
-    # phone = db.Column(db.Numeric(10, 0))
-    # cuisine_description = db.Column(db.String(60))
-    # inspection_date = db.Column(db.Date())
-    # action = db.Column(db.Text())
-    # violation_code = db.Column(db.String(3))
-    # violation_description = db.Column(db.Text())
-    # critical_flag = db.Column(db.String(25))
-    # score = db.Column(db.Numeric(3, 0))
-    # grade = db.Column(db.String(2))
-    # grade_date = db.Column(db.Date())
-    # record_date = db.Column(db.Date())
-    # inspection_type = db.Column(db.Text())
-    # latitude = db.Column(db.Numeric(16, 14))
-    # longitude = db.Column(db.Numeric(16, 14))
-    # community_board = db.Column(db.Numeric(3, 0))
-    # council_district = db.Column(db.Numeric(3, 0))
-    # census_tract = db.Column(db.Numeric(5, 0))
+if __name__ == "__main__":
+    cli()
 
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 import pandas as pds
+from markupsafe import escape
+from sqlalchemy import create_engine, text
 
 import os
 
@@ -21,8 +23,13 @@ db = SQLAlchemy(app)
 
 
 @app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+def home_page():
+    return """ 
+    """
+    
+
+# def hello_world():
+    # return jsonify(hello="world")
 
 
 @app.route("/static/<path:filename>")
@@ -45,7 +52,22 @@ def upload_file():
     <!doctype html>
     <title>upload new File</title>
     <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
+      <p><input type=file name=file><input type=submit value=Upload></P>
     </form>
     """
+
+@app.route("/load_data", methods=["GET", "POST"])
+def reload_data():
+    DATABASE_URI = "postgresql://postgresuser:poster@db:5432/restaurant_db"
+    db.engine.execute(f'DROP TABLE IF EXISTS restaurants;')
+    db.session.commit()
+    df = pds.read_csv("restaurants.csv")
+    print(df.shape)
+    engine = create_engine(DATABASE_URI)
+    df.to_sql("restaurants", engine, index=False, if_exists="replace")
+    df_output = df.to_html(max_rows=200)
+    db.session.commit()
+    df_column_count = 26
+    text = "<!doctype html> <title>Loaded Database</title> <br> <p>Records in the database: {0} </p> <br> <br> {1}"
+    return text.format(df.size / df_column_count, df_output)
 
